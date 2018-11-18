0a247ee25ec95a7804214c1c0f4089b35bb13c77
eng-edu <eduardo.eng15@hotmail.com>
2018-11-18 17:18:11 -0200

    up

--- app/src/main/java/com/developer/edu/arco/controller/ControllerCadastro.java
+++ app/src/main/java/com/developer/edu/arco/controller/ControllerCadastro.java
@@ -1,8 +1,15 @@
 package com.developer.edu.arco.controller;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Base64;
 import android.widget.Toast;
+
 import com.developer.edu.arco.conectionAPI.ConfigRetrofit;
+import com.developer.edu.arco.view.ActCadastro;
+
+import java.io.ByteArrayOutputStream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +23,10 @@ public class ControllerCadastro {
 
         String result = "S3MS3NH4";
 
+        final ProgressDialog dialog = new ProgressDialog(context);
+        dialog.setTitle("Aguarde...");
+        dialog.setCancelable(false);
+        dialog.show();
 
         if (nome.length() > 0
                 && nome.length() > 0
@@ -34,23 +45,26 @@ public class ControllerCadastro {
             stringCall.enqueue(new Callback<String>() {
                 @Override
                 public void onResponse(Call<String> call, Response<String> response) {
-                    if(response.code() == 201){
+                    if (response.code() == 201) {
                         //chama o controller login
-                        new ControllerLogin().logar(context, email,senha, discente, docente);
-
-                    }else if (response.code() == 405) {
+                        new ControllerLogin().logar(context, email, senha, discente, docente);
+                        dialog.dismiss();
+                    } else if (response.code() == 405) {
                         Toast.makeText(context, response.body(), Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
                     }
                 }
 
                 @Override
                 public void onFailure(Call<String> call, Throwable t) {
                     Toast.makeText(context, t.getMessage(), Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
                 }
             });
 
         } else {
             Toast.makeText(context, "Preencha todos os campos!", Toast.LENGTH_LONG).show();
+            dialog.dismiss();
         }
 
     }
--- app/src/main/java/com/developer/edu/arco/view/ActCadastro.java
+++ app/src/main/java/com/developer/edu/arco/view/ActCadastro.java
@@ -1,11 +1,24 @@
 package com.developer.edu.arco.view;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,12 +26,36 @@ import android.widget.TextView;
 import com.developer.edu.arco.R;
 import com.developer.edu.arco.controller.ControllerCadastro;
 
+import java.io.ByteArrayOutputStream;
+
 public class ActCadastro extends AppCompatActivity {
+    public static final int IMAGEM_INTERNA = 12;
+
+    ImageView fotoPerfil;
+    static String fotoBase64 = "";
+
+
+    public static String getFotoBase64() {
+        return fotoBase64;
+    }
+
+    public static void setFotoBase64(String fotoBase64) {
+        ActCadastro.fotoBase64 = fotoBase64;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+        if (ActivityCompat.checkSelfPermission(ActCadastro.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //e caso ainda não tenha dado, ele solicita...
+            ActivityCompat.requestPermissions(ActCadastro.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
 
         ActionBar bar = getSupportActionBar();
         bar.setTitle("CADASTRO");
@@ -54,8 +91,62 @@ public class ActCadastro extends AppCompatActivity {
                         senha.getText().toString(),
                         discente.isChecked(),
                         docente.isChecked());
+
+                Log.i("DEVEDU", fotoBase64);
             }
         });
 
+
+        fotoPerfil = (ImageView) findViewById(R.id.perfil);
+        fotoPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pegarImg(v);
+            }
+        });
+
+
     }
+
+    public void pegarImg(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGEM_INTERNA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        if (requestCode == IMAGEM_INTERNA && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.perfil);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+         //   setFotoBase64(getEncoded64ImageStringFromBitmap(BitmapFactory.decodeFile(picturePath)));
+
+        }
+
+
+    }
+
+
+    public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteFormat = stream.toByteArray();
+        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
+        return imgString;
+    }
+
 }
